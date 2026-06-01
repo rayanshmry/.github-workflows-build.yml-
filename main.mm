0bc5f135193db379c13d3eb91b1ce29b9fe38b9c
rayanshmry <mba142900@gmail.com>
2026-06-01 05:46:26 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,85 +1,58 @@
 #import <UIKit/UIKit.h>
 
-// متغيرات عامة
 static UIButton *floatingBtn = nil;
+static UIView *menuView = nil;
 static NSTimer *clickTimer = nil;
-static float clickInterval = 0.5;
 
-@interface ClickerManager : NSObject
-+ (void)createFloatingButton;
-+ (void)showMenu;
+@interface MoustashController : NSObject
++ (void)setup;
 @end
 
-@implementation ClickerManager
+@implementation MoustashController
 
-+ (void)createFloatingButton {
-    UIWindow *window = [UIApplication sharedApplication].keyWindow;
-    if (!window) window = [[UIApplication sharedApplication].windows firstObject];
++ (void)setup {
+    UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
     
-    // زر متوسط الحجم (60x60)
+    // الزر العائم
     floatingBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-    floatingBtn.frame = CGRectMake(20, 200, 60, 60);
+    floatingBtn.frame = CGRectMake(20, 100, 60, 60);
     floatingBtn.backgroundColor = [UIColor blackColor];
-    floatingBtn.layer.cornerRadius = 30;
+    floatingBtn.layer.cornerRadius = 10;
     floatingBtn.layer.borderWidth = 2;
     floatingBtn.layer.borderColor = [UIColor blueColor].CGColor;
-    floatingBtn.layer.masksToBounds = YES;
-    
-    // شكل الزر (تاج واسم)
-    [floatingBtn setTitle:@"👑\nم" forState:UIControlStateNormal];
-    floatingBtn.titleLabel.font = [UIFont boldSystemFontOfSize:10];
+    [floatingBtn setTitle:@"👑\nموستاش" forState:UIControlStateNormal];
+    floatingBtn.titleLabel.font = [UIFont systemFontOfSize:10];
     floatingBtn.titleLabel.numberOfLines = 2;
-    floatingBtn.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [floatingBtn addTarget:self action:@selector(toggleMenu) forControlEvents:UIControlEventTouchUpInside];
+    [keyWindow addSubview:floatingBtn];
     
-    // تفعيل التحريك (سحب الزر)
-    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
-    [floatingBtn addGestureRecognizer:pan];
+    // القائمة المربعة (مخفية في البداية)
+    menuView = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 200, 180)];
+    menuView.backgroundColor = [UIColor colorWithWhite:0 alpha:0.9];
+    menuView.hidden = YES;
     
-    // تفعيل الضغط
-    [floatingBtn addTarget:self action:@selector(showMenu) forControlEvents:UIControlEventTouchUpInside];
+    UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake(20, 30, 160, 30)];
+    UIButton *startBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    startBtn.frame = CGRectMake(20, 80, 70, 40);
+    [startBtn setTitle:@"تشغيل" forState:UIControlStateNormal];
     
-    [window addSubview:floatingBtn];
-    [window bringSubviewToFront:floatingBtn];
+    UIButton *stopBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    stopBtn.frame = CGRectMake(110, 80, 70, 40);
+    [stopBtn setTitle:@"إيقاف" forState:UIControlStateNormal];
+    
+    [menuView addSubview:slider];
+    [menuView addSubview:startBtn];
+    [menuView addSubview:stopBtn];
+    [keyWindow addSubview:menuView];
 }
 
-+ (void)handlePan:(UIPanGestureRecognizer *)p {
-    UIView *v = p.view;
-    CGPoint t = [p translationInView:v.superview];
-    v.center = CGPointMake(v.center.x + t.x, v.center.y + t.y);
-    [p setTranslation:CGPointZero inView:v.superview];
++ (void)toggleMenu {
+    menuView.hidden = !menuView.hidden;
 }
-
-+ (void)showMenu {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"🤖 أوتو موستاش" 
-                                                                    message:@"السرعة (ثانية/نقرة)" 
-                                                             preferredStyle:UIAlertControllerStyleAlert];
-    
-    [alert addTextFieldWithConfigurationHandler:^(UITextField *tf) {
-        tf.placeholder = @"0.5";
-        tf.keyboardType = UIKeyboardTypeDecimalPad;
-        tf.text = [NSString stringWithFormat:@"%.1f", clickInterval];
-    }];
-    
-    [alert addAction:[UIAlertAction actionWithTitle:@"▶️ تشغيل" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a) {
-        clickInterval = [alert.textFields[0].text floatValue];
-        [clickTimer invalidate];
-        clickTimer = [NSTimer scheduledTimerWithTimeInterval:clickInterval repeats:YES block:^(NSTimer *t) {
-            // تنفيذ النقرة
-            [[UIApplication sharedApplication] sendEvent:[UIEvent new]];
-        }];
-    }]];
-    
-    [alert addAction:[UIAlertAction actionWithTitle:@"⏹ إيقاف" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *a) {
-        [clickTimer invalidate];
-    }]];
-    
-    [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
-}
-
 @end
 
-__attribute__((constructor)) static void init() {
+__attribute__((constructor)) static void load() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [ClickerManager createFloatingButton];
+        [MoustashController setup];
     });
 }
