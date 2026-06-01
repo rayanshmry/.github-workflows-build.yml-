d7d21df3e753edad0ecd6c9ae11930da9500f8fb
rayanshmry <mba142900@gmail.com>
2026-06-01 06:08:18 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,87 +1,98 @@
 #import <UIKit/UIKit.h>
+#include <IOKit/hid/IOHIDEventSystem.h>
+#include <mach/mach_time.h>
+#include <pthread.h>
 
-// المتغيرات
-static UIButton *floatingBtn = nil;
-static UIView *menuView = nil;
-static NSTimer *clickTimer = nil;
-static float clickSpeed = 0.5;
+// --- نظام محاكاة النقرات عالي الدقة ---
+typedef struct __IOHIDEvent *IOHIDEventRef;
+extern IOHIDEventRef IOHIDEventCreateDigitizerFingerEvent(CFAllocatorRef allocator, uint64_t timestamp, uint32_t index, uint32_t identity, uint32_t eventMask, float x, float y, float pressure, float range, bool touching, bool rangeValid);
+extern void IOHIDEventSystemClientDispatchEvent(void *client, IOHIDEventRef event);
 
-@interface MoustashFix : NSObject
-+ (void)renderAll;
-+ (void)toggleMenu;
+// --- فئة مدير النقرات (Click Manager) ---
+@interface ClickManager : NSObject
+@property (nonatomic, assign) float interval;
+@property (nonatomic, assign) int repeatCount;
+@property (nonatomic, assign) BOOL isRunning;
++ (instancetype)shared;
+- (void)startExecutionWithPoint:(CGPoint)point;
+- (void)stopExecution;
 @end
 
-@implementation MoustashFix
+@implementation ClickManager
++ (instancetype)shared { static ClickManager *s; static dispatch_once_t t; dispatch_once(&t, ^{ s = [ClickManager new]; }); return s; }
 
-+ (void)renderAll {
-    UIWindow *window = [UIApplication sharedApplication].keyWindow ?: [[UIApplication sharedApplication].windows firstObject];
+- (void)startExecutionWithPoint:(CGPoint)point {
+    self.isRunning = YES;
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        while(self.isRunning) {
+            void *client = IOHIDEventSystemClientCreate(kCFAllocatorDefault);
+            uint64_t ts = mach_absolute_time();
+            // الضغط
+            IOHIDEventRef down = IOHIDEventCreateDigitizerFingerEvent(kCFAllocatorDefault, ts, 1, 1, 1, point.x, point.y, 1.0, 1.0, true, true);
+            IOHIDEventSystemClientDispatchEvent(client, down);
+            // الرفع
+            IOHIDEventRef up = IOHIDEventCreateDigitizerFingerEvent(kCFAllocatorDefault, ts, 1, 1, 1, point.x, point.y, 0.0, 1.0, false, true);
+            IOHIDEventSystemClientDispatchEvent(client, up);
+            CFRelease(down); CFRelease(up); CFRelease(client);
+            [NSThread sleepForTimeInterval:self.interval];
+        }
+    });
+}
+- (void)stopExecution { self.isRunning = NO; }
+@end
+
+// --- فئة الواجهة الرسومية المعقدة (UI Architect) ---
+@interface ModMenuUI : UIView
+@property (nonatomic, strong) UIView *container;
+@end
+
+@implementation ModMenuUI
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    self.backgroundColor = [UIColor colorWithWhite:0.1 alpha:0.9];
+    self.layer.cornerRadius = 25;
     
-    // الزر العائم مع نظام "إعادة الظهور"
-    if (!floatingBtn) {
-        floatingBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-        floatingBtn.frame = CGRectMake(30, 150, 60, 60);
-        floatingBtn.backgroundColor = [UIColor blackColor];
-        floatingBtn.layer.cornerRadius = 30;
-        floatingBtn.layer.borderWidth = 2;
-        floatingBtn.layer.borderColor = [UIColor blueColor].CGColor;
-        [floatingBtn setTitle:@"👑" forState:UIControlStateNormal];
-        [floatingBtn addTarget:self action:@selector(toggleMenu) forControlEvents:UIControlEventTouchUpInside];
-        
-        UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(dragBtn:)];
-        [floatingBtn addGestureRecognizer:pan];
-        [window addSubview:floatingBtn];
-    }
-    [window bringSubviewToFront:floatingBtn];
-
-    // القائمة (المربعة القابلة للتحريك)
-    if (!menuView) {
-        menuView = [[UIView alloc] initWithFrame:CGRectMake(100, 150, 200, 250)];
-        menuView.backgroundColor = [UIColor colorWithWhite:0.1 alpha:0.95];
-        menuView.layer.cornerRadius = 20;
-        menuView.userInteractionEnabled = YES; // تفعيل اللمس
-        
-        // إمكانية تحريك القائمة
-        UIPanGestureRecognizer *panMenu = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(dragMenu:)];
-        [menuView addGestureRecognizer:panMenu];
-        
-        // شريط السرعة
-        UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake(20, 50, 160, 30)];
-        slider.minimumValue = 0.1; slider.maximumValue = 2.0; slider.value = clickSpeed;
-        [slider addTarget:self action:@selector(updateSpeed:) forControlEvents:UIControlEventValueChanged];
-        [menuView addSubview:slider];
-        
-        // زر التشغيل
-        UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-        btn.frame = CGRectMake(50, 120, 100, 40);
-        [btn setTitle:@"تشغيل/إيقاف" forState:UIControlStateNormal];
-        [btn addTarget:self action:@selector(toggleClick) forControlEvents:UIControlEventTouchUpInside];
-        [menuView addSubview:btn];
-        
-        menuView.hidden = YES;
-        [window addSubview:menuView];
-    }
+    // بناء القائمة (بمستوى الـ AutoTouch)
+    [self buildHeader];
+    [self buildSliders];
+    return self;
 }
 
-+ (void)dragBtn:(UIPanGestureRecognizer *)p { [p.view setCenter:[p locationInView:p.view.superview]]; }
-+ (void)dragMenu:(UIPanGestureRecognizer *)p { [p.view setCenter:[p locationInView:p.view.superview]]; }
-+ (void)toggleMenu { menuView.hidden = !menuView.hidden; }
-+ (void)updateSpeed:(UISlider *)s { clickSpeed = s.value; }
+- (void)buildHeader {
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(0, 10, self.frame.size.width, 30)];
+    title.text = @"Moustash Pro Engine";
+    title.textAlignment = NSTextAlignmentCenter;
+    title.textColor = [UIColor cyanColor];
+    [self addSubview:title];
+}
 
-+ (void)toggleClick {
-    if (clickTimer) { [clickTimer invalidate]; clickTimer = nil; }
-    else {
-        clickTimer = [NSTimer scheduledTimerWithTimeInterval:clickSpeed repeats:YES block:^(NSTimer *t) {
-            // نقرات حقيقية
-            CGPoint pos = CGPointMake(200, 400); 
-            [[UIApplication sharedApplication].keyWindow hitTest:pos withEvent:nil];
-        }];
-    }
+- (void)buildSliders {
+    UISlider *s = [[UISlider alloc] initWithFrame:CGRectMake(20, 60, 260, 30)];
+    [s addTarget:self action:@selector(sliderChanged:) forControlEvents:UIControlEventValueChanged];
+    [self addSubview:s];
+}
+
+- (void)sliderChanged:(UISlider *)s {
+    [ClickManager shared].interval = s.value;
 }
 @end
 
-__attribute__((constructor)) static void init() {
-    // رادار لإعادة رسم الزر كل 2 ثانية لضمان عدم اختفائه
-    [NSTimer scheduledTimerWithTimeInterval:2.0 repeats:YES block:^(NSTimer *t) {
-        [MoustashFix renderAll];
-    }];
+// --- فئة النظام الأساسي (Kernel Hooking) ---
+@interface MoustashCore : NSObject
++ (void)initializeSystem;
+@end
+
+@implementation MoustashCore
++ (void)initializeSystem {
+    UIWindow *win = [UIApplication sharedApplication].keyWindow;
+    ModMenuUI *menu = [[ModMenuUI alloc] initWithFrame:CGRectMake(50, 100, 300, 400)];
+    [win addSubview:menu];
+}
+@end
+
+// --- نقطة الدخول (Entry Point) ---
+__attribute__((constructor)) static void bootstrap() {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [MoustashCore initializeSystem];
+    });
 }
