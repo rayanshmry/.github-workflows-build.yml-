e21eb84d7aae313ee5fa17b7304937307824c8a1
rayanshmry <mba142900@gmail.com>
2026-06-01 05:14:12 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,26 +1,65 @@
 #import <UIKit/UIKit.h>
 
-// الكود الاحترافي مع قائمة منبثقة
+// تعريف المؤقت والمستوى
+static NSTimer *clickTimer = nil;
+static float clickSpeed = 1.0; // السرعة الافتراضية
+static UIButton *floatingBtn = nil;
+
+// دالة محاكاة اللمس الحقيقي
+void performClick(CGPoint point) {
+    // هذا الكود يرسل حدث لمس للنظام (IOHID) لمحاكاة نقرة حقيقية
+    // ملاحظة: هذا يتطلب صلاحيات، وسيعمل داخل أغلب الألعاب
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIWindow *window = [UIApplication sharedApplication].keyWindow;
+        UIView *hitView = [window hitTest:point withEvent:nil];
+        [hitView touchesBegan:[NSSet setWithObject:[UITouch new]] withEvent:nil];
+        [hitView touchesEnded:[NSSet setWithObject:[UITouch new]] withEvent:nil];
+    });
+}
+
 __attribute__((constructor)) static void setup() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIWindow *window = [UIApplication sharedApplication].keyWindow;
         
-        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
-        btn.frame = CGRectMake(20, 100, 70, 70);
-        [btn setTitle:@"👨🏻‍🦰" forState:UIControlStateNormal];
-        btn.backgroundColor = [UIColor blackColor];
-        btn.layer.cornerRadius = 35;
-        [btn addTarget:nil action:@selector(showMenu) forControlEvents:UIControlEventTouchUpInside];
-        [window addSubview:btn];
+        floatingBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        floatingBtn.frame = CGRectMake(20, 150, 80, 80);
+        floatingBtn.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.8];
+        floatingBtn.layer.cornerRadius = 40;
+        [floatingBtn setTitle:@"👨🏻‍🦰" forState:UIControlStateNormal];
+        [floatingBtn addTarget:nil action:@selector(showMenu) forControlEvents:UIControlEventTouchUpInside];
+        
+        UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
+        [floatingBtn addGestureRecognizer:pan];
+        
+        [window addSubview:floatingBtn];
     });
 }
 
++ (void)handlePan:(UIPanGestureRecognizer *)p {
+    UIView *v = p.view;
+    CGPoint t = [p translationInView:v.superview];
+    v.center = CGPointMake(v.center.x + t.x, v.center.y + t.y);
+    [p setTranslation:CGPointZero inView:v.superview];
+}
+
 void showMenu() {
-    UIWindow *w = [UIApplication sharedApplication].keyWindow;
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"موستاش برو" message:@"اختر سرعة النقرات" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"أوتو موستاش" message:@"تحكم بالسرعة" preferredStyle:UIAlertControllerStyleAlert];
     
-    [alert addAction:[UIAlertAction actionWithTitle:@"سريع جداً" style:UIAlertActionStyleDefault handler:nil]];
-    [alert addAction:[UIAlertAction actionWithTitle:@"إيقاف" style:UIAlertActionStyleCancel handler:nil]];
+    [alert addTextFieldWithConfigurationHandler:^(UITextField *tf) {
+        tf.placeholder = @"السرعة (ثانية للنقرة)";
+        tf.keyboardType = UIKeyboardTypeDecimalPad;
+    }];
+
+    [alert addAction:[UIAlertAction actionWithTitle:@"بدء" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a) {
+        clickSpeed = [alert.textFields[0].text floatValue] ?: 1.0;
+        clickTimer = [NSTimer scheduledTimerWithTimeInterval:clickSpeed repeats:YES block:^(NSTimer *t) {
+            performClick(CGPointMake(100, 100)); // مكان النقرة الافتراضي
+        }];
+    }]];
     
-    [w.rootViewController presentViewController:alert animated:YES completion:nil];
+    [alert addAction:[UIAlertAction actionWithTitle:@"إيقاف" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *a) {
+        [clickTimer invalidate];
+    }]];
+    
+    [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
 }
